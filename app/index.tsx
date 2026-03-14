668be945406490289da4456e8eccac6f1c7cca88
Tamanna Sharma <ts.harma2882006@gmail.com>
2026-03-14 12:49:46 +0530

    tailwind autocompletion fixed

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
 	return (
 		<View className="flex-1 justify-center items-center">
-			<Text className="text-5xl text-red-300">I love you raj betuu.</Text>
+			<Text className="text-5xl text-red-600">I love you adddaaa.</Text>
 		</View>
 	);
 }
